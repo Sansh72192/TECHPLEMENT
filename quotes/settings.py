9403a9b225d2695a46d5c569b58c7da2cb916018
Sansh72192 <anshsingh4as@gmail.com>
2025-05-14 21:07:33 +0530

    Add Render domain to ALLOWED_HOSTS

--- quotes/settings.py
+++ quotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-62-0tp==o555i1skv$gdvlnygj2xo6k(n!%)&=ybj+$y8r+^2w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quotesapp2g.onrender.com']
+ALLOWED_HOSTS = ['quotesapp2-bdz0.onrender.com']
 
 
 
