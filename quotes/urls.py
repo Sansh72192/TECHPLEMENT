800c1e4410dbcc08d69bdb3e3f62b17c274d9d80
Sansh72192 <anshsingh4as@gmail.com>
2025-05-14 22:04:14 +0530

    Fix: Added root URL handling and redirect to /home/

--- quotes/urls.py
+++ quotes/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quotesapp.urls')),
+    path('', RedirectView.as_view(url='/home/')),  # <-- redirect base URL to /home/
+    path('', include('quotesapp.urls')),           # <-- include your app URLs
 ]
--- quotesapp/urls.py
+++ quotesapp/urls.py
@@ -1,9 +1,11 @@
 # quotesapp/urls.py
+# project_root/urls.py
+# quotesapp/urls.py
 from django.urls import path
 from .views import index_view, get_quote
-from quotesapp import views
 
 urlpatterns = [
-    path('home/', index_view, name='home'),
-    path('get-quote/',views.get_quote, name='get_quote'),
+    path('', index_view, name='home'),            # <-- this will handle base path
+    path('home/', index_view, name='home'),       # still allows /home/
+    path('get-quote/', get_quote, name='get_quote'),
 ]
